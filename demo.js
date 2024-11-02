c2c0f19c859b2d0ff1a7d5cdebc794394862e9f3
Frank Anthony <frankez478@gmail.com>
2024-11-02 04:42:31 +0100

    Major changes to the homepage

--- demo.js
+++ demo.js
@@ -1,3 +1,36 @@
-function greet(){
-    console.log('HEllo')
-}
+let show = document.getElementById("ham");
+let nav = document.getElementById("nav-links");
+let day = document.getElementById("days");
+let hrs = document.getElementById("hrs");
+let mins = document.getElementById("mins");
+let secs = document.getElementById("secs");
+
+show.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+
+let fade = document.querySelector(".fade");
+let observe = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+observe.observe(fade);
+
+let d = new Date();
+days.innerHTML = d.getUTCDay();
+hrs.innerHTML = d.getHours();
+mins.innerHTML = d.getMinutes();
+secs.innerHTML = d.getSeconds();
+
+// window.addEventListener('scroll', () => {
+//   const fades = document.querySelectorAll('.fade');
+//   fades.forEach(fade => {
+//     const fadeTop = fade.getBoundingClientRect().top;
+//     if (fadeTop < window.innerHeight - 100) {
+//       fade.classList.add('visible');
+//     }
+//   });
+// });
